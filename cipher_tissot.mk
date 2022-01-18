e856cd57ba86411d7377c5f4d249f98ae12dc9c4
ajitlenka30 <ajitlenka30@gmail.com>
2022-01-18 11:55:30 +0530

    tissot: Update build fingerprint from redfin SQ1A.220105.002
    Signed-off-by: ajitlenka30 <ajitlenka30@gmail.com>

--- cipher_tissot.mk
+++ cipher_tissot.mk
@@ -40,4 +40,4 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="tissot-user 8.0.0 OPR1.170623.026 8.1.10 release-keys"
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := "xiaomi/tissot/tissot_sprout:8.0.0/OPR1.170623.026/8.1.10:user/release-keys"
+BUILD_FINGERPRINT := "google/redfin/redfin:12/SQ1A.220105.002/7961164:user/release-keys"
